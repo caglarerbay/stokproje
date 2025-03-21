9dedd78f894c631e7c7d0a7d27b612be2833acd1
Caglar Erbay <caglarerbay@gmail.com>
2025-03-22 00:15:04 +0300

    Yeni özellik eklendi

---
+++ inventory_mobile/lib/home_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Ana Ekran')),
+      body: Center(
+        child: Text('Hoş geldiniz!', style: TextStyle(fontSize: 20)),
+      ),
+    );
+  }
+}

403bda9b52a1485c30284b079d8c76dfeb537c9c
Caglar Erbay <caglarerbay@gmail.com>
2025-03-22 00:19:03 +0300

    Yeni özellik eklendi

--- inventory_mobile/lib/main.dart
+++ inventory_mobile/lib/main.dart
@@ -6,7 +6,7 @@ import 'login_screen.dart';
 // Eğer forgot_password_screen.dart varsa import et
 import 'forgot_password_screen.dart';
 // Eğer home_screen.dart varsa import et
-// import 'home_screen.dart';
+import 'home_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
         '/login': (context) => LoginScreen(),
         '/register': (context) => RegisterScreen(),
         '/forgot_password': (context) => ForgotPasswordScreen(),
-        // '/home': (context) => HomeScreen(),
+        '/home': (context) => HomeScreen(),
       },
       // Eğer “/home” gibi bir ekrana gideceksen, oraya da widget eklemen lazım
       // Bu satırlar senin diğer dosyalarında tanımlı widget'lara işaret etmeli

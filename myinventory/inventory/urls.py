24181fd38a60e5d91e4f540d55e6038f1ae592e6
Caglar Erbay <caglarerbay@gmail.com>
2025-03-22 11:55:55 +0300

    Yeni özellik eklendi

--- myinventory/inventory/urls.py
+++ myinventory/inventory/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('password_change/', auth_views.PasswordChangeView.as_view(template_name='inventory/password_change.html'),
          name='password_change'),
     path('password_change/done/',
-         auth_views.PasswordChangeDoneView.as_view(template_name='inventory/password_change_done.html'),
+         auth_views.PasswordChangeDoneView.as_view(template_name='inventory/password_change_done.html'),787
          name='password_change_done'),
 
     path('add_product/', views.add_product, name='add_product'),
